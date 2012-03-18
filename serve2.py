c41be07a4fae4f442882943052b93861342841a9
Brandon Kienle <kienlebr@gmail.com>
2012-03-18 19:27:38 -0400

    threading and robust recieves working! excellent.

--- serve2.py
+++ serve2.py
@@ -4,41 +4,43 @@ import os
 import socket
 import miniapp
 
+import time
+import threading
+
 def handle_connection(sock):
     print 'handling connection...'
-    try:
-        print 'recieving data...'
-        data = ''
-        recieved = sock.recv(1024)
-        if(recieved):
-            while(1):
-                data += recieved
-                if len(recieved) < 1024:
-                    break
-                else:
-                    recieved = sock.recv(1024)
-        elif not data:
-            print 'no data recieved'
+    index = 0;
+    endset = "\r\n\r\n"
+    recieved = ''
+    data = ''
+    while 1:
+        try:
+            recieved = sock.recv(1)
+            data += recieved;
+            if recieved == endset[index]:
+                index+=1
+            else:
+                index = 0
+            if index == 4:
+                break;
+            if not data:
+                print 'no data recieved'
+                break
+
+        except socket.error:
             return
-        print 'data recieved...'
-        print ''
-        print data
-        print ''
 
-        #print 'data:', (data,)
-        data = miniapp.format_return(data)
+    #print 'data:', (data,)
+    data = miniapp.format_return(data)
 
-        #print 'data:', (data,)
-        data = str(data)
-        #print 'data:', (data,)
+    #print 'data:', (data,)
+    data = str(data)
+    #print 'data:', (data,)
 
-        sock.sendall(data)
-        print "data sent"
-        sock.close()
-
-    except socket.error:
-        return
-print "Done"
+    sock.sendall(data)
+    print "data sent"
+    sock.close()
+    print "Done"
 
 if __name__ == '__main__':
     #interface, port = sys.argv[1:3]
@@ -50,7 +52,7 @@ if __name__ == '__main__':
     sock = socket.socket()
     sock.bind( (interface, port) )
     sock.listen(5)
-
+    #threads = []
     while 1:
         print 'waiting...'
         (client_sock, client_address) = sock.accept()
